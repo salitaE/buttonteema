89b3d5eb115079dfab6e70be44e7514ab688eb92
Salla Devaa <estradasalla@gmail.com>
2021-12-08 23:42:01 +0200

    Muutokset opettajan pohjaan

---
+++ src/HeaderButton.js
@@ -0,0 +1,12 @@
+import React, {useContext} from 'react';
+import ThemeContext from './ThemeContext';
+
+function HeaderButton() {
+  return (
+    <div>
+      <button style={useContext(ThemeContext)}>Press me</button>
+    </div>
+  );
+}
+
+export default HeaderButton;
